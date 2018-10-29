9cbd3ab65feb7a43b35d8ac459cab7590eed7daf
Daniel <daniel.ortizp91@gmail.com>
2018-10-29 17:57:42 -0600

    Add Tufflex logo

--- src/containers/es/Products.js
+++ src/containers/es/Products.js
@@ -1,20 +1,11 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 import {
-  Button,
   Container,
-  Divider,
   Grid,
   Header,
-  Icon,
   Image,
-  List,
-  Menu,
-  Responsive,
   Segment,
-  Sidebar,
-  Visibility,
-  Flag
 } from 'semantic-ui-react'
 import DesktopContainer from './DesktopContainer'
 import Footer from './Footer'
@@ -25,12 +16,13 @@ import Knauf from './../images/knauf.png'
 import Silvercote from './../images/logo-silvercote.png'
 import Insudry from './../images/insudry.png'
 import GMX from './../images/gmx.png'
+import Tufflex from './../images/tufflex.png'
 
 const ResponsiveContainer = ({ children, urlString }) => (
   <div>
-    <DesktopContainer
-      urlString={urlString}
-      >{children}</DesktopContainer>
+    <DesktopContainer urlString={urlString}>
+      {children}
+    </DesktopContainer>
   </div>
 )
 
@@ -42,13 +34,13 @@ const PageLayout = () => (
   <ResponsiveContainer
     urlString="products">
     <Container text textAlign="center">
-      <Header as='h1' style={{ fontSize: '2em', padding:"1em 0em", paddingTop: "2em" }}>
-        NUESTROS <span style={{color:"#2185d0"}}>PRODUCTOS</span>
+      <Header as="h1" style={{ fontSize: '2em', padding: '1em 0em', paddingTop: '2em' }}>
+        NUESTROS <span style={{ color: '#2185d0' }}>PRODUCTOS</span>
         {/* <Divider style={{color:"#2185d0"}}/> */}
         <hr style={{
-          width: "10%",
-          border: "1px solid #2185d0",
-        }}/>
+          width: '10%',
+          border: '1px solid #2185d0',
+        }} />
       </Header>
     </Container>
     <Grid
@@ -60,68 +52,68 @@ const PageLayout = () => (
      >
       <Grid.Row>
         <Grid.Column>
-          <Image href="http://insudry.com/" src={Insulation} centered size="large"/>
+          <Image href="http://insudry.com/" src={Insulation} centered size="large" />
           <Segment basic >
-              <Header as="h3">AISLAMIENTO TÉRMICO Y ACÚSTICO</Header>
-              <hr style={{
-                width: "10%",
-                border: "1px solid #2185d0",
-                marginLeft: 0,
-                padding: '0% 1em'
-              }}/>
-              <Header as='h5'>Representante de ventas exclusivo en Latinoamérica
-                 de los productos de Asilamiento Knauf North America. </Header>
-                <Grid>
-                  <Grid.Row columns={2}>
-                    <Grid.Column>
-                      <Image href="http://www.knauf.es/" centered src={Knauf} size='huge' />
-                    </Grid.Column>
-                    <Grid.Column>
-                      <Image href="https://www.silvercote.com/" centered src={Silvercote} size='huge' />
-                    </Grid.Column>
-                  </Grid.Row>
-                </Grid>
-          </Segment>
-        </Grid.Column>
-          <Grid.Column>
-            <Image src={Other} centered size="large"/>
-            <Segment basic >
-            <Header as="h3">RECUBRIMIENTOS A PRUEBA DE AGUA</Header>
+            <Header as="h3">AISLAMIENTO TÉRMICO Y ACÚSTICO</Header>
             <hr style={{
-              width: "10%",
-              border: "1px solid #2185d0",
+              width: '10%',
+              border: '1px solid #2185d0',
               marginLeft: 0,
-              padding: '0% 1em'
+              padding: '0% 1em',
             }}/>
-            <Header as='h5'>Distribución de productos de revestimiento,
-              tapetes acústicos, etc. </Header>
-            </Segment>
+            <Header as="h5">Representante de ventas exclusivo en Latinoamérica de los productos de Asilamiento Knauf North America. </Header>
             <Grid>
               <Grid.Row columns={2}>
                 <Grid.Column>
-                  <Image href="http://insudry.com/" centered src={Insudry} size='huge' />
+                  <Image href="http://www.knauf.es/" centered src={Knauf} size="huge" />
                 </Grid.Column>
                 <Grid.Column>
-                  <Image href="https://www.gmxco.com/about/about-us.html" centered src={GMX} size='huge'/>
+                  <Image href="https://www.silvercote.com/" centered src={Silvercote} size="huge" />
                 </Grid.Column>
               </Grid.Row>
             </Grid>
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={Glass} centered size="large"/>
-            <Segment basic >
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Image src={Other} centered size="large"/>
+          <Segment basic >
+            <Header as="h3">RECUBRIMIENTOS A PRUEBA DE AGUA</Header>
+            <hr style={{
+              width: '10%',
+              border: '1px solid #2185d0',
+              marginLeft: 0,
+              padding: '0% 1em',
+            }}/>
+            <Header as="h5">Distribución de productos de revestimiento, tapetes acústicos, etc. </Header>
+          </Segment>
+          <Grid>
+            <Grid.Row columns={3}>
+              <Grid.Column>
+                <Image href="http://insudry.com/" centered src={Insudry} size="huge" />
+              </Grid.Column>
+              <Grid.Column>
+                <Image href="https://www.gmxco.com/about/about-us.html" centered src={GMX} size="huge" />
+              </Grid.Column>
+              <Grid.Column>
+                <Image href="https://www.tufflexpolymers.com/" centered src={Tufflex} size="huge" />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Grid.Column>
+        <Grid.Column>
+          <Image src={Glass} centered size="large" />
+          <Segment basic >
             <Header as="h3">VIDRIO Y OTROS MATERIALES</Header>
             <hr style={{
-              width: "10%",
-              border: "1px solid #2185d0",
+              width: '10%',
+              border: '1px solid #2185d0',
               marginLeft: 0,
-              padding: '0% 1em'
-            }}/>
-            <Header as='h5'>Distribuimos vidrio de alta calidad a precios
-              competitivos.</Header>
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
+              padding: '0% 1em',
+            }} />
+            <Header as="h5">Distribuimos vidrio de alta calidad a precios competitivos.</Header>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
     <Footer />
   </ResponsiveContainer>
