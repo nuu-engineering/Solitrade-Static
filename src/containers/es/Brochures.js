8d1f854d199655f7a986e5c2abef8a4bb6232720
Daniel <daniel.ortizp91@gmail.com>
2019-08-19 11:32:04 -0500

    Add custom brochure

--- src/containers/es/Brochures.js
+++ src/containers/es/Brochures.js
@@ -2,27 +2,14 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import {
   Container,
-  Divider,
   Grid,
   Header,
-  Icon,
   Image,
-  List,
-  Menu,
-  Card,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility,
-  Flag
 } from 'semantic-ui-react'
 import { withRouteData } from 'react-static'
 
 import DesktopContainer from './DesktopContainer'
 import Footer from './Footer'
-import Other from './../images/otherproducts.png'
-import Glass from './../images/glass.png'
-import Insulation from './../images/insulation.png'
 import Knauf from './../images/knauf.png'
 import Silvercote from './../images/logo-silvercote.png'
 import Insudry from './../images/insudry.png'
@@ -32,7 +19,9 @@ const ResponsiveContainer = ({ children, urlString }) => (
   <div>
     <DesktopContainer
       urlString={urlString}
-      >{children}</DesktopContainer>
+    >
+      {children}
+    </DesktopContainer>
   </div>
 )
 
@@ -44,34 +33,105 @@ const PageLayout = ({ brochuresNames }) => (
   <ResponsiveContainer
     urlString="brochures">
     <Container text textAlign="center">
-      <Header as='h1' style={{ fontSize: '2em', padding:"0em 0em", paddingTop: "2em" }}>
-        FOL<span style={{color:"#2185d0"}}>LETOS</span>
+      <Header as="h1" style={{ fontSize: '2em', padding:"0em 0em", paddingTop: "2em" }}>
+        FOL<span style={{ color: '#2185d0' }}>LETOS</span>
         {/* <Divider style={{color:"#2185d0"}}/> */}
-        <hr style={{
-          width: '10%',
-          border: '1px solid #2185d0',
-        }}/>
+        <hr
+          style={{
+            width: '10%',
+            border: '1px solid #2185d0',
+          }}
+        />
       </Header>
     </Container>
     <Grid
       container
       stackable
-      divided="vertically"
-      style={{ padding: '3% 0%', paddingBottom: "8em" }}
+      style={{
+        padding: '3% 0%',
+        paddingBottom: '8em',
+      }}
       columns="4"
      >
       <Grid.Row>
+        <Header as="h2">Folleto de Solitrade</Header>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column />
+        <Grid.Column
+          width={10}
+        >
+          <div
+            style={{
+              width: '100%',
+              height: 0,
+              paddingBottom: '66%',
+              position: 'relative',
+            }}
+          >
+            <iframe
+              src="https://cdn.flipsnack.com/widget/v2/widget.html?hash=fvtkehxbl"
+              width="100%"
+              height="100%"
+              seamless="seamless"
+              scrolling="no"
+              frameBorder="0"
+              allowFullScreen
+              title="Solitrade Product Catalog 2019"
+              style={{
+                position: 'absolute',
+              }}
+            />
+          </div>
+        </Grid.Column>
+        <Grid.Column />
+      </Grid.Row>
+
+      <Grid.Row>
+        <Header
+          as="h2"
+          style={{
+            marginTop: '3em',
+          }}
+        >
+          Otros Folletos
+        </Header>
+      </Grid.Row>
+
+      <Grid.Row>
         <Grid.Column>
-          <Image href='/es/brochures/knauf' centered src={Knauf} size="huge" />
+          <Image
+            href="/es/brochures/knauf"
+            centered
+            src={Knauf}
+            size="huge"
+          />
         </Grid.Column>
         <Grid.Column>
-          <Image href='/es/brochures/silvercote' centered src={Silvercote} size="huge" />
+          <Image
+            href="/es/brochures/silvercote"
+            centered
+            src={Silvercote}
+            size="huge"
+          />
         </Grid.Column>
         <Grid.Column>
-          <Image href='/es/brochures/insudry' centered src={Insudry} size="huge" />
+          <Image
+            href="/es/brochures/insudry"
+            centered
+            src={Insudry}
+            size="huge"
+          />
         </Grid.Column>
         <Grid.Column>
-          <Image href='/es/brochures/gmx' centered src={GMX} size="huge" style={{ padding: '0em -30em' }}/>
+          <Image
+            href="/es/brochures/gmx"
+            centered
+            src={GMX}
+            size="huge"
+            style={{ padding: '0em -30em' }}
+          />
         </Grid.Column>
       </Grid.Row>
     </Grid>
